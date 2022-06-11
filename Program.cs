4f18714167c072551185118e65a2df8c66f44e01
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-11 14:36:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class combines related variables (fields) and functions (methods)
+
+            int number;
+            //Person person = new Person(); // instead of name of the class you can use
+            //operator var:
+            var person = new Person();
+            person.Name = "Abe";
+            person.Introduce();
+
+            int result = Calculator.Add(1, 2);
+            Console.WriteLine(result);
+            
+        }
+    }
+    public class Person
+    {
+        public string Name;
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, my name is " + Name);
+        }
+    }
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
