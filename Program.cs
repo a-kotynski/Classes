fb50bac9a4f1bf352cc87be3382e64a66179aade
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-12 11:43:59 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -15,11 +15,11 @@ namespace Classes
             int number;
             //Person person = new Person(); // instead of name of the class you can use
             //operator var:
-            var person = new Person();
+            var person = new Person(); // creating an object from class
             person.Name = "Abe";
             person.Introduce();
 
-            int result = Calculator.Add(1, 2);
+            int result = Calculator.Add(1, 2); // accessing method directly from class
             Console.WriteLine(result);
             
         }
@@ -34,7 +34,8 @@ namespace Classes
     }
     public class Calculator
     {
-        public static int Add(int a, int b)
+        public static int Add(int a, int b) // static allows to access the method directly
+                                            // from Calculator class
         {
             return a + b;
         }
